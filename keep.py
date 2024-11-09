48800edad5fdb0434d32d7f7aaf3a01bfec18910
dengligedd <142644737+lirui11223@users.noreply.github.com>
2024-11-09 13:22:11 +0800

    keep.py

--- keep.py
+++ keep.py
@@ -1,15 +1,7 @@
-import easygui
 from PIL import Image,ImageFilter,ImageDraw,ImageFont
 
-def keep(path):
+def keep(path,color):
     img = Image.open(path)
-    color_choice = easygui.choicebox('请选择要保留的通道','my_picture',['保留红色通道','保留绿色通道','保留蓝色通道'])
-    if color_choice == '保留红色通道':
-        color = 'r'
-    elif color_choice == '保留绿色通道':
-        color = 'g'
-    elif color_choice == '保留蓝色通道':
-        color = 'b'
     w = img.width
     h = img.height
     for x in range(w):
